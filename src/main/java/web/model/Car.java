7a0a7b991a79826abd4e1b1f76e8266efac587f9
Bogdan Bulgakov <Bog.bulgakov@yandex.ru>
2023-12-15 22:21:10 +0300

    готово

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,35 @@
+package web.model;
+
+public class Car {
+    private String model;
+    private int series;
+    private long mileage;
+    public Car(String model, int series, long mileage) {
+        this.model = model;
+        this.series = series;
+        this.mileage = mileage;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public void setMileage(long mileage) {
+        this.mileage = mileage;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public long getMileage() {
+        return mileage;
+    }
+}
